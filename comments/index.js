8ace872f330e366db06b81e06b900c6d8dbe49b2
SUPHOT NARAPONG <suphot.n@gmail.com>
2024-10-06 15:58:01 +0700

    comment api making

--- comments/index.js
+++ comments/index.js
@@ -8,10 +8,17 @@ app.use(bodyParser.json());
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
   
 });
 
 app.post('/posts/:id/comments', (req, res) => {
+  const commentsId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({id: commentsId, content}); 
+  commentsByPostId[req.params.id] = comments; 
+  res.status(201).send(comments);
 
 });
 
