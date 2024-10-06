4866bbad46b200c78de86593b4bfa36605edf7f9
SUPHOT NARAPONG <suphot.n@gmail.com>
2024-10-06 15:44:01 +0700

    initial commit

---
+++ comments/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+
+});
+
+app.listen(4001, () => {
+  console.log('Server running on port 4001');
+});
